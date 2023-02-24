81e3cdebbbf603feeeb441f8deb80af14d8222e5
valeraus <vlaus0@gmail.com>
2023-02-24 17:51:35 +0200

    Добавьте файлы проекта.

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    /*
+    Використовуючи Visual Studio, створіть проект за шаблоном Console Application.
+    Потрібно:
+    Створити масив розмірністю N елементів, заповнити його довільними цілими значеннями.
+    Вивести найбільше значення масиву, найменше значення масиву, загальну суму елементів, 
+    середнє арифметичне всіх елементів, вивести всі непарні значення.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
